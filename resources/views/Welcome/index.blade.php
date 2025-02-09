1af6f7af1509ed8cd59b86d1f6f7f402d539908d
airjuice1 <airjuice1@gmail.com>
2025-02-09 16:09:38 +0300

    add welcome controller and welcome views

---
+++ resources/views/Welcome/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ex. Consequatur repellendus ad quasi dignissimos debitis perspiciatis delectus est aliquid exercitationem esse facilis fugit eaque nesciunt aut, corrupti animi rem.
+</div>
